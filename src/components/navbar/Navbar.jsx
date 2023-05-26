c5966112958823f0bb83ffe6167c77999294e292
elvis <elvismulaj12@gmail.com>
2023-05-26 22:12:15 +0200

    kerko filmin
    Kerko film front

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,25 +7,25 @@ export const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="container">
-            <div className="left">
-            <img 
-            src='https://filma365.net/wp-content/uploads/2022/09/ss.png'
-            alt='elvisi'/>
+              <div className="left">
+                  <img 
+                  src='https://filma365.net/wp-content/uploads/2022/09/ss.png'
+                  alt='elvisi'/>
 
-            <span>Ballina</span>
-            <span>Seriale</span>
-            <span>Filma</span>
-            <span>Te Rejat</span>
-            <span>Lista ime</span>
-            <span>homepage</span>
+                  <span>Ballina</span>
+                  <span>Seriale</span>
+                  <span>Filma</span>
+                  <span>Te Rejat</span>
+                  <span>Lista ime</span>
+                  <span>homepage</span>
 
-            </div>
+              </div>
             <div className="right">
-            <span>Elvis</span>
-            <Notifications/>
-            <img src='https://yt3.googleusercontent.com/ytc/AL5GRJXLE4TfrvZ6RlSNilncmTtgD8KzxoL0QuDEuJQ64_A=s900-c-k-c0x00ffffff-no-rj' alt='profil-picture'/>
-            <ArrowDropDown/>
-            <input type='text' placeholder='search'/>
+              <span>Elvis</span>
+              <Notifications/>
+              <img src='https://yt3.googleusercontent.com/ytc/AL5GRJXLE4TfrvZ6RlSNilncmTtgD8KzxoL0QuDEuJQ64_A=s900-c-k-c0x00ffffff-no-rj' alt='profil-picture'/>
+              <ArrowDropDown/>
+              <input type='text' placeholder='Kerko Filmin'/>
             </div>
         </div>
      </div>
