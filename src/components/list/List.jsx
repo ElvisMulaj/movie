8f9fecab5f1a293879a1e7c3f8bee7524382c727
elvis <elvismulaj12@gmail.com>
2023-05-30 12:41:45 +0200

    kodimi i sliderit

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./list.scss"
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from '@mui/icons-material'
 import ListItem from '../listItem/ListItem'
 
 export default function List() {
+
+  const [isMoved,setIsMoved]= useState(false);
+  const [sliderNumber,setSlideNumber]= useState(0);
+ 
+
+ const listRef= useRef()
+
+//funksioni i sliderit 
+  const handleClick = (direction) =>{
+    setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x -50
+    if(direction ==="left" &&sliderNumber >0 ){
+      setSlideNumber(sliderNumber-1)
+      listRef.current.style.transform =`translateX(${230 + distance}px)`
+    }
+    if(direction ==="right" && sliderNumber <2){
+      setSlideNumber(sliderNumber +1)
+      listRef.current.style.transform =`translateX(${-230 + distance}px)`
+    }
+  }
+ 
+  
   return (
     <div className="list">
        <span className="listTitle"> Continue to watch</span>
-       <div className="wraper">
-        <ArrowBackIosOutlined/>
-        <div className="container">
+       <div className="wrapper">
+        <ArrowBackIosOutlined 
+        className="sliderArrow left"
+         onClick ={() => handleClick("left")}
+         style={{display: !isMoved && "none"}}
+         />
+        <div className="container" ref={listRef}>
             <ListItem/>
             <ListItem/>
             <ListItem/>
@@ -20,9 +46,9 @@ export default function List() {
             <ListItem/>
             <ListItem/>
             <ListItem/>
-            <ListItem/>
+            
         </div>
-        <ArrowForwardIosOutlined/>
+        <ArrowForwardIosOutlined className="sliderArrow right" onClick ={() => handleClick("right")}/>
 
        </div>
         </div>
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -4,7 +4,7 @@ import "./listItem.scss"
 export default function ListItem() {
   return (
     <div className="listItem">
-        item
+        <img src="https://images.immediate.co.uk/production/volatile/sites/3/2020/09/Interstellar-4ace2e9.jpeg" alt="" />
     </div>
   )
 }
